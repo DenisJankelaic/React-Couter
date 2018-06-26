689bca2c4b3df9e5a12f49ae85874e1d66cb79d4
Denis Jankelaic <d3nziss@gmail.com>
2018-06-26 14:18:07 +0300

    Fixed Counter

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,40 +2,50 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 interface State {
-    count: number
+    count: number;
+    name: string;
 }
 
 interface Props {
-
 }
 
 class App extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            count: 1
+            count: 1,
+            name: ""
+        };
+    }
+
+    protected plusOne: React.MouseEventHandler<HTMLDivElement> = () => {
+        this.setState(state => ({
+            count: state.count + 1
+        }));
+    }
+
+    protected minusOne: React.MouseEventHandler<HTMLDivElement> = () => {
+        if (this.state.count <= 0) {
+            count: 0;
         }
+        else {
+            this.setState(state =>({
+                count: state.count - 1
+            }));
+        }
+
     }
 
-    protected plusOne: React.MouseEventHandler<HTMLDivElement>=() => {
-        this.setState({
-            count: this.state.count + 1
-        });
-    }
-
-    protected minusOne: React.MouseEventHandler<HTMLDivElement>=() =>  {
-        this.setState({
-            count: this.state.count - 1
-        });
-    }
-
-    protected resetCounter: React.MouseEventHandler<HTMLDivElement>=() =>  {
+    protected resetCounter: React.MouseEventHandler<HTMLDivElement> = () => {
         this.setState({
             count: 0
         });
     }
 
-    protected setCounter(event: React.ChangeEvent<HTMLInputElement>) {
+    protected setCounter: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        if (this.state.count <= 0) {
+            count: 0;
+        }
         this.setState({
             count: Number(event.target.value)
         });
@@ -44,7 +54,8 @@ class App extends React.Component<Props, State> {
     public render(): JSX.Element {
         return (
             <div>
-                <div>Counter: <input onChange= {(event: React.ChangeEvent<HTMLInputElement>) => this.setCounter(event)} type="number" value={this.state.count}/></div>
+                <div>Counter: {this.state.count}</div>
+                <div><input onChange={this.setCounter} type="text" value={this.state.count} /></div>
                 <div onClick={this.plusOne}> Plus </div>
                 <div onClick={this.minusOne}> Minus </div>
                 <div onClick={this.resetCounter}> Reset </div>
